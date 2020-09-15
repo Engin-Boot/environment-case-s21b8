3cd8f46a7356cb6e88d5005da25f0d85fa5524b3
ApoorvaK1998 <66967956+ApoorvaK1998@users.noreply.github.com>
2020-09-15 20:38:40 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 pushd "%~dp0"
 
-C:/Users/320087877/source/repos/EnvironmentalMonitoringReciever/EnvironmentalMonitoringReciever/bin/debug/EnvironmentalMonitoringReciever.exe
+EnvironmentalMonitoringReciever/bin/debug/EnvironmentalMonitoringReciever.exe
 
 popd
