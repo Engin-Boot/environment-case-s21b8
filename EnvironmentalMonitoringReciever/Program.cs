dfead3fc0b8eeaca998a33caf81c753d6682eed8
Apoorva K <320087877@INGBTCPIC5NBW71>
2020-09-16 10:13:33 +0530

    modification

---
+++ EnvironmentalMonitoringReciever/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EnvironmentalMonitoringReciever
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+           
+            ValueChecker valueChecker = new ValueChecker();
+            Console.Write("Enter Integer: ");
+            string val = Console.ReadLine();
+            int a = Convert.ToInt32(val);
+            Console.WriteLine("Your input: {0}", a);
+
+            
+            valueChecker.Temperature(a);
+            valueChecker.Humidity(a);
+
+
+
+        }
+    }
+}
---
+++ EnvironmentalMonitoringReciever/ValueChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvironmentalMonitoringReciever
+{
+    class ValueChecker
+    {
+        public void Temperature(int TemperatureValue)
+        {
+            TemperatureAlert temperatureAlert = new TemperatureAlert();
+            if (TemperatureValue>=37&&TemperatureValue<40)
+            {
+                temperatureAlert.TemperatureHighWarningLevel();
+            }
+            else if(TemperatureValue>40)
+            {
+                temperatureAlert.TemperatureHighErrorLevel();
+            }
+           if(TemperatureValue>=0&&TemperatureValue<4)
+            {
+                temperatureAlert.TemperatureLowWarningLevel();
+
+            }
+            else if(TemperatureValue<0)
+            {
+                temperatureAlert.TemperatureLowErrorLevel();
+            }
+          
+               
+        }
+        public void Humidity(float HumidityValue)
+        {
+            HumidityAlert humidityAlert = new HumidityAlert();
+            if(HumidityValue>=70)
+            {
+                humidityAlert.HumidityHighWarningLevel();
+            }
+            else if(HumidityValue>=90)
+            {
+                humidityAlert.HumidityHighErrorLevel();
+            }
+            
+
+        }
+    }
+}
