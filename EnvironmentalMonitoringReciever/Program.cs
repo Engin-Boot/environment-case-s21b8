ee9148c5728d7010158aed67486e8e891b3b20f6
Apoorva K <320087877@INGBTCPIC5NBW71>
2020-09-17 20:48:02 +0530

    changes

--- EnvironmentalMonitoringReciever/Program.cs
+++ EnvironmentalMonitoringReciever/Program.cs
@@ -3,19 +3,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 
+
 namespace EnvironmentalMonitoringReciever
 {
     class Program
     {
-<<<<<<< HEAD
-        static List<string> inputvalues=new List<string>();
-        
-        static void Main(string[] args)
-=======
-       
+        static List<string> inputvalues = new List<string>();
 
-        static void Main()
->>>>>>> 38ff5323619894b59ff3311a1eb4fdfc29aad8ff
+        static void Main(string[] args)
         {
             string s;
             while ((s = Console.ReadLine()) != null)
@@ -25,50 +20,28 @@ namespace EnvironmentalMonitoringReciever
             }
             ReadConsoleOutput rd = new ReadConsoleOutput();
             rd.ParseInputList();
-            
+
         }
-<<<<<<< HEAD
-        
+
         public class ReadConsoleOutput
         {
-            
+
             public int val;
             internal void temperatureread(string input)
-=======
-        public class OutputCapture : TextWriter
-        {
-            private readonly TextWriter _stdOutWriter;
-            public TextWriter Captured { get; }
-            public override Encoding Encoding => Encoding.ASCII;
-
-            public OutputCapture()
             {
-                this._stdOutWriter = Console.Out;
-                Console.SetOut(this);
-                Captured = new StringWriter();
-            }
+                string tmp = input.Split(':')[1].TrimStart();
 
-            public override void Write(string output)
->>>>>>> 38ff5323619894b59ff3311a1eb4fdfc29aad8ff
-            {
-                string tmp=input.Split(':')[1].TrimStart();
-                
                 if ((tmp.Equals(Convert.ToInt32(tmp))))
                 {
                     Console.WriteLine("Value Not Present");
                 }
                 else
                 {
-                    
+
                     ValueChecker value = new ValueChecker();
                     value.Temperature(val);
                 }
-                
-<<<<<<< HEAD
-=======
-                Captured.Write(output);
-                _stdOutWriter.Write(output);
->>>>>>> 38ff5323619894b59ff3311a1eb4fdfc29aad8ff
+
             }
             internal void humidityread(string input)
             {
@@ -77,30 +50,22 @@ namespace EnvironmentalMonitoringReciever
                 ValueChecker value = new ValueChecker();
                 value.Humidity(val);
 
-<<<<<<< HEAD
             }
             public void ParseInputList()
             {
-                foreach(var input in inputvalues)
+                foreach (var input in inputvalues)
                 {
-                    if(input.StartsWith("Temperature"))
+                    if (input.StartsWith("Temperature"))
                     {
                         ReadConsoleOutput rd = new ReadConsoleOutput();
                         rd.temperatureread(input);
                     }
-                    if(input.StartsWith("Humidity"))
+                    if (input.StartsWith("Humidity"))
                     {
                         ReadConsoleOutput rd = new ReadConsoleOutput();
                         rd.humidityread(input);
                     }
                 }
-=======
-            public override void WriteLine(string output)
-            {
-               
-                Captured.WriteLine(output);
-                _stdOutWriter.WriteLine(output);
->>>>>>> 38ff5323619894b59ff3311a1eb4fdfc29aad8ff
             }
 
         }
@@ -118,46 +83,46 @@ namespace EnvironmentalMonitoringReciever
         //        this.output = output;
         //    }
 
-            
+
         //}
-            //private TextWriter stdOutWriter;
-            //public TextWriter Captured { get; private set; }
-            //public void OutputCapture()
-            //{
+        //private TextWriter stdOutWriter;
+        //public TextWriter Captured { get; private set; }
+        //public void OutputCapture()
+        //{
 
-            //    this.stdOutWriter = Console.Out;
-            //    Console.SetOut(this);
-            //    Captured = new StringWriter();
-            //}
+        //    this.stdOutWriter = Console.Out;
+        //    Console.SetOut(this);
+        //    Captured = new StringWriter();
+        //}
 
-            //public class OutputCapture : TextWriter, IDisposable
-            //{
-            //    private TextWriter stdOutWriter;
-            //    public TextWriter Captured { get; private set; }
-            //    public override Encoding Encoding { get { return Encoding.ASCII; } }
+        //public class OutputCapture : TextWriter, IDisposable
+        //{
+        //    private TextWriter stdOutWriter;
+        //    public TextWriter Captured { get; private set; }
+        //    public override Encoding Encoding { get { return Encoding.ASCII; } }
 
-            //    public OutputCapture()
-            //    {
-            //        this.stdOutWriter = Console.Out;
-            //        Console.SetOut(this);
-            //        Captured = new StringWriter();
-            //    }
+        //    public OutputCapture()
+        //    {
+        //        this.stdOutWriter = Console.Out;
+        //        Console.SetOut(this);
+        //        Captured = new StringWriter();
+        //    }
 
-            //    override public void Write(string output)
-            //    {
+        //    override public void Write(string output)
+        //    {
 
-            //        Captured.Write(output);
-            //        stdOutWriter.Write(output);
-            //    }
+        //        Captured.Write(output);
+        //        stdOutWriter.Write(output);
+        //    }
 
-            //    override public void WriteLine(string output)
-            //    {
+        //    override public void WriteLine(string output)
+        //    {
 
-            //        Captured.WriteLine(output);
-            //        stdOutWriter.WriteLine(output);
-            //    }
+        //        Captured.WriteLine(output);
+        //        stdOutWriter.WriteLine(output);
+        //    }
 
 
-            //}
-        }
+        //}
+    }
 }
--- Sender/Program.cs
+++ Sender/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Sender
+﻿using System;
+
+namespace Sender
 {
     class Program
     {
@@ -8,18 +10,18 @@
             string validTestPath = @"C:\Users\320087877\OneDrive - Philips\Documents\EM\TemperatureHumiditySampleData.csv";
 
             HelperFunctions csvFile = new HelperFunctions();
-<<<<<<< HEAD
+
             string ReadFileWithHeader_status = csvFile.ReadFileWithHeader(validTestPath);
             string ExtractDelimiterSeparatedValues_status = csvFile.ExtractDelimiterSeparatedValues(',');
             string CheckAndReplaceEmptyValues_status = csvFile.CheckAndReplaceEmptyValues();
             string RedirectOutputToConsole_status = csvFile.RedirectFileOutputToConsole();
             Console.WriteLine(ReadFileWithHeader_status);
-=======
+
             //var readFileWithHeader = csvFile.ReadFileWithHeader(validTestPath);
             //var extractDelimiterSeparatedValues = csvFile.ExtractDelimiterSeparatedValues(',');
             var checkAndReplaceEmptyValues = csvFile.CheckAndReplaceEmptyValues();
             //var redirectFileOutputToConsole = csvFile.RedirectFileOutputToConsole();
->>>>>>> 38ff5323619894b59ff3311a1eb4fdfc29aad8ff
+
         }
     }
 }
