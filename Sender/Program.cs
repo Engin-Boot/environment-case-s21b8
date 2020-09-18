4d0482b5d28feebc68ef5c8548cf1a7836f33672
Maurya <Aruna.Maurya@philips.com>
2020-09-18 14:23:45 +0530

    made some changes

--- Sender/Program.cs
+++ Sender/Program.cs
@@ -5,13 +5,13 @@
         static void Main()
         {
             //string path = "";
-            string validTestPath = @"D:\a\environment-case-s21b8\environment-case-s21b8\TemperatureHumiditySampleData.csv";
-
+            //string validTestPath = @"D:\a\environment-case-s21b8\environment-case-s21b8\TemperatureHumiditySampleData.csv";
+            string validTestPath = @"C:\Users\320087992\OneDrive - Philips\Desktop\LaunchCase-Study\TemperatureHumiditySampleData";
             HelperFunctions csvFile = new HelperFunctions();
-            //var readFileWithHeader = csvFile.ReadFileWithHeader(validTestPath);
-            //var extractDelimiterSeparatedValues = csvFile.ExtractDelimiterSeparatedValues(',');
+            var readFileWithHeader = csvFile.ReadFileWithHeader(validTestPath);
+            var extractDelimiterSeparatedValues = csvFile.ExtractDelimiterSeparatedValues(',');
             var checkAndReplaceEmptyValues = csvFile.CheckAndReplaceEmptyValues();
-            //var redirectFileOutputToConsole = csvFile.RedirectFileOutputToConsole();
+            var redirectFileOutputToConsole = csvFile.RedirectFileOutputToConsole();
         }
     }
 }
